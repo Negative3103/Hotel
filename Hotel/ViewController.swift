21dec85d8626696d2e03fadca7fa672fa465db09
Negative3103 <xasand3103@gmail.com>
2023-09-07 12:41:07 +0500

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Хасан Давронбеков on 07/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
